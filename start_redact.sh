9e74615efa00e9dc12d3f5749cdcc177109b7c69
simonbrighter <simon.weinhold@brighter.ai>
2021-12-02 16:10:50 +0100

    move readiness check to docker compose

--- start_redact.sh
+++ start_redact.sh
@@ -10,7 +10,6 @@ export REDACT_UTILS_IMAGE="brighter.azurecr.io/redact-utils:0.12.2"
 
 export REDACT_API_PORT=8787
 export REDACT_UI_PORT=8786
-export REDACT_INFER_HEALTH_PORT=8000
 
 export REDACT_LICENSE_FILE="./license.bal"
 
@@ -25,14 +24,6 @@ fi
 # start containers
 export HOST_IP=$(hostname -I | awk '{print $1}')
 docker-compose -f docker-compose.yaml up --force-recreate -d --remove-orphans
-
-# wait for redact-infer container to become ready
-url="http://127.0.0.1:${REDACT_INFER_HEALTH_PORT}/v2/health/ready"
-httpd=`curl -A "Redact Check" -sL --connect-timeout 3 -w "%{http_code}\n" ${url} -o /dev/null` || echo "Waiting for redact-infer container to become ready ..."
-until [[ "${httpd}" == "200" ]]; do
-  sleep 30
-  httpd=`curl -A "Redact Check" -sL --connect-timeout 3 -w "%{http_code}\n" ${url} -o /dev/null` || echo "Waiting for redact-infer container to become ready ..."
-done
 
 # print some urls
 echo "redact API running at http://${HOST_IP}:${REDACT_API_PORT}"
