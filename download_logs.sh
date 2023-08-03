792f766b0e350ac100c13eac946e86ead420fe14
fredyeah <mark.s@catalyst-students.com>
2023-08-03 14:27:55 +0200

    chore: add log download script

---
+++ download_logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -a
+source docker-compose.env
+export HOST_IP=$(hostname -I | awk '{print $1}')
+set +a
+
+curl "http://${HOST_IP}:${REDACT_UI_PORT}/services/v3/errorlog" --output redact_logs.zip
