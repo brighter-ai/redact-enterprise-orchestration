3549d7747719ac9d69fac292aadd8dd1661798ec
fredyeah <mark.s@catalyst-students.com>
2023-08-03 14:30:22 +0200

    fix: use api port

--- download_logs.sh
+++ download_logs.sh
@@ -6,4 +6,4 @@ source docker-compose.env
 export HOST_IP=$(hostname -I | awk '{print $1}')
 set +a
 
-curl "http://${HOST_IP}:${REDACT_UI_PORT}/services/v3/errorlog" --output redact_logs.zip
+curl "http://${HOST_IP}:${REDACT_API_PORT}/services/v3/errorlog" --output redact_logs.zip
