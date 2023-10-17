61eb1426568c3933e3640b7a369ea448348edd17
Georg Koester <georg.koester@brighter.ai>
2023-10-17 09:36:36 +0200

    Fix: Fixes of space handling, console detaching, and warnings.
    * start_redact default behavior changed: Console attached.
    * Whitespace in installation dir could have caused problems.
    * Handling of installation dir variable led to unnecessary warnings.

--- stop_redact.sh
+++ stop_redact.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -o errexit
 set -a
-installation_dir=${INSTALLATION_DIR}
 if [ -z $installation_dir ]; then
-    installation_dir="."
+    installation_dir="$(realpath '.')"
+    export INSTALLATION_DIR="${installation_dir}"
+else
+    installation_dir="$INSTALLATION_DIR"
 fi
-source $installation_dir/docker-compose.env
+source "$installation_dir/docker-compose.env"
 set +a
 
 # start containers
 export HOST_IP=$(hostname -I | awk '{print $1}')
-docker compose -f $installation_dir/docker-compose.yaml down --remove-orphans
+docker compose -f "$installation_dir/docker-compose.yaml" down --remove-orphans
