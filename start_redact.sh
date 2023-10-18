13ad48b59e89d834034bacfafc68c3661d07df08
Frederic Scheer <frederic@brighter.ai>
2023-10-18 17:33:10 +0200

    fix: add fi

--- start_redact.sh
+++ start_redact.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -o errexit
 set -a
-# systemd service provides installation_dir as an environment variable 
+# systemd service provides installation_dir as an environment variable
 installation_dir=${INSTALLATION_DIR}
 if [ -z $installation_dir ]; then
     installation_dir="$(realpath '.')"
     export INSTALLATION_DIR="${installation_dir}"
-
+fi
 source "$installation_dir/docker-compose.env"
 set +a
 
